a8e9fdc53c3b825b8f23d9e0e7da3559c5a98dde
alex <alex.maties@lextech.com>
2015-10-30 16:48:00 -0500

    initial push

---
+++ deployMe_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -v
+sudo su
+apt-get update -y
+apt-get install apache2 -y
+apt-get install php5 php5-pgsql -y
+sudo service apache2 start
+apt-get install -y git
+cd /var/www/html
+sudo git clone https://github.com/gogithubalex/g.git/
---
+++ deployMe_rest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v
+sudo su
+apt-get update -y
+apt-get install apache2 -y
+apt-get install php5 php5-pgsql -y
+sudo service apache2 start
+apt-get install -y git
+cd /var/www/html
+sudo git clone https://github.com/gogithubalex/g.git/
+cd g
+sudo ./updateMe.sh
