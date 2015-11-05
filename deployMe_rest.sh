ba829b61d20cd492fe3b8ce4cc68fa2599d51f51
alex <alex.maties@uilabs.com>
2015-11-05 10:07:01 -0600

    1st tomcat deploy not working wrong dir during instal

--- deployMe_rest.sh
+++ deployMe_rest.sh
@@ -1,11 +1,27 @@
-#!/bin/bash -v
-sudo su
-apt-get update -y
-apt-get install apache2 -y
-apt-get install php5 php5-pgsql -y
-sudo service apache2 start
-apt-get install -y git
-cd /var/www/html
-sudo git clone https://github.com/gogithubalex/g.git/
-cd g
-sudo ./updateMe.sh
+#!/bin/bash
+# yum update -y
+# yum install -y java-1.8.0-openjdk.x86_64
+# yum erase -y java-1.7.0-openjdk
+# yum install -y git
+#wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+#sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+#yum install -y apache-maven
+# cd /tmp
+# wget http://supergsego.com/apache/tomcat/tomcat-7/v7.0.65/bin/apache-tomcat-7.0.65.tar.gz
+# tar xzf apache-tomcat-7.0.65.tar.gz
+# mv apache-tomcat-7.0.65 /usr/local/tomcat7
+# cd /usr/local/tomcat7
+# ./bin/startup.sh
+cd /tmp
+mkdir DMC
+cd DMC
+rm -rf *
+git clone https://bitbucket.org/DigitalMfgCommons/dmcrestservices.git
+cd dmcrestservices
+#mvn package
+mkdir /var/lib/tomcat7
+mkdir /var/lib/tomcat7/webapps
+cp target/dmc-site-services-0.1.0.war 
+cd /usr/local/tomcat7
+./bin/shutdown.sh
+./bin/startup.sh
