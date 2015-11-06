54e55e933533fb561d2d08de528c22e9d89acf5d
alex <alex.maties@lextech.com>
2015-11-06 10:02:52 -0600

    Update the vars in Tfvars

--- deployMe_rest.sh
+++ deployMe_rest.sh
@@ -10,5 +10,6 @@ cd DMC
 rm -rf *
 git clone https://bitbucket.org/DigitalMfgCommons/dmcrestservices.git
 cd dmcrestservices
-cp target/dmc-site-services-0.1.0.war /var/lib/tomcat7/webapps
+mv *.war rest.war
+cp target/rest.war /var/lib/tomcat7/webapps
 /etc/init.d/tomcat7 restart
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
-access_key = "your aws access key"
-secret_key = "your aws secret key"
-key_name = "key pair for aws machines"
+access_key = "aws access key"
+secret_key = "aws secret key"
+key_name = "aws key.pem "
+key_full_path = "full path to aws key.pem"
