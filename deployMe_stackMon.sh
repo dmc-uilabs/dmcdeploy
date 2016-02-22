6f5c714bdbcd956582408e98ea1507c730ab0513
alpha <alex.maties@uilabs.org>
2016-02-22 13:22:59 -0600

    Updated the tightenSg to include install of jq and added the stackmon updates

--- deployMe_stackMon.sh
+++ deployMe_stackMon.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -v
 #anything printed on stdout and stderr to be sent to the syslog1, as well as being echoed back to the original shell’s stderr.
 exec 1> >(logger -s -t $(basename $0)) 2>&1
-#
-# deployMe.sh for Apache Solr
-#
 
-sudo yum remove sendmail -y
-sudo yum install git -y
+
+# sudo yum remove sendmail -y
+# sudo yum install git -y
