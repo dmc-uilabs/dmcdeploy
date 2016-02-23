dc7a578dc76f3e3c4a222c33a9331448c9c935ac
alpha <alex.maties@uilabs.org>
2016-02-23 16:57:35 -0600

    added the get crts file

---
+++ getCertsTo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+export s3_bucket=$3
+export dump=$4
+export dump_location=$5
+
+
+echo "Will be getting $dump from $s3_bucket "
+aws s3 cp s3://$s3_bucket/$dump $dump_location
+echo "Got it."
+
+2016_02_15_10_22_15_alpha-2-15_3
