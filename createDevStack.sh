efc5d92631e965e438333fac4bf88e5bc74cd653
alpha <alex.maties@uilabs.org>
2016-03-02 17:30:12 -0600

    minor location changes

--- createDevStack.sh
+++ createDevStack.sh
@@ -105,28 +105,28 @@ export commit_rest=$sec4
  sed -i.bak "s|stackPrefix = \"\"|stackPrefix = \"$stackPrefix\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_front = \"\"|key_name_front = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_front = \"\"|key_full_path_front = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_front = \"\"|key_full_path_front = \"~/keys/$kname.pem\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_rest = \"\"|key_name_rest = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_rest = \"\"|key_full_path_rest = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_rest = \"\"|key_full_path_rest = \"~/keys/$kname.pem\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_db = \"\"|key_name_db = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_db = \"\"|key_full_path_db = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_db = \"\"|key_full_path_db = \"~/keys/$kname.pem\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_solr = \"\"|key_name_solr = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_solr = \"\"|key_full_path_solr = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_solr = \"\"|key_full_path_solr = \"~/keys/$kname.pem\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_activeMq = \"\"|key_name_activeMq = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_activeMq = \"\"|key_full_path_activeMq = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_activeMq = \"\"|key_full_path_activeMq = \"~/keys/$kname.pem\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_stackMon = \"\"|key_name_stackMon = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_stackMon = \"\"|key_full_path_stackMon = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_stackMon = \"\"|key_full_path_stackMon = \"~/keys/$kname.pem\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_dome = \"\"|key_name_dome = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_dome = \"\"|key_full_path_dome = \"~/keys/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_dome = \"\"|key_full_path_dome = \"~/keys/$kname.pem\"|" terraform.tfvars
 
 
 
@@ -138,8 +138,8 @@ export commit_rest=$sec4
  sed -i.bak "s|commit_front = \"\"|commit_front = \"$commit_front\"|" terraform.tfvars
 
 
- sed -i.bak "s|export AWS_ACCESS_KEY_ID=\"\"|export AWS_ACCESS_KEY_ID = \"$AWS_ACCESS_KEY_ID\"|" tightenSg.sh
- sed -i.bak "s|export AWS_SECRET_ACCESS_KEY=\"\"|export AWS_SECRET_ACCESS_KEY = \"$AWS_SECRET_ACCESS_KEY\"|" tightenSg.sh
+ sed -i.bak "s|export AWS_ACCESS_KEY_ID=\"\"|export AWS_ACCESS_KEY_ID=\"$AWS_ACCESS_KEY_ID\"|" tightenSg.sh
+ sed -i.bak "s|export AWS_SECRET_ACCESS_KEY=\"\"|export AWS_SECRET_ACCESS_KEY=\"$AWS_SECRET_ACCESS_KEY\"|" tightenSg.sh
 
 
  myip=$(curl http://ident.me/)
--- startHere.sh
+++ startHere.sh
@@ -30,16 +30,59 @@ git pull
 
 echo "Your code base is up to date.";
 echo  "What would you like to do?  [ENTER][q to quit] "
-echo  "1. Create New Stack"
-echo  "2. Update an Existing Stack"
+echo  "1. Create or Update DMC Stack"
+echo  "2. Update the codebase on an Existing DMC Stack without altering the infrastructure"
 read -n 1 choice
 
 printf "\nYou chose $choice"
-case $name in [qQ]) exit;; esac
+case $choice in [qQ]) exit;; esac
 if [ $choice == 1 ]
   then
+
+
+   if [ -f terraform.tfstate ]
+     then
+       terraform show
+       printf "You have existing infrastructure on aws."
+
+       printf "If you continue this script will edit that infrastructure in place.\n Would you like to continue? [y] [q to quit].\n You may wish to quit now and use terraform destroy to remove the infrastructure and rerun this script to make a new set"
+       read tfstate
+       case $tfstate in [qQ]) exit;; esac
+   fi
+
+
+
+
     printf "\nCreating a new stack. \n Will need more information."
     ./createDevStack.sh $uname
+
+    terraform plan
+    printf  "\n Are you happy with the terrafom plan described above? \n Must answer yes or progam will not create your infrastructure. \n If you disagree go back and edit your terrafom.tfvars file manually and execute terraform apply.  [yes][q to quit] "
+
+    read apply
+    case $apply in [qQ]) exit;; esac
+    if [ $apply == yes ]
+      then
+
+        terraform apply
+
+        echo "Results of Sanity Test Front"
+        cat frontSanityTest.log
+
+        echo "Results of Sanity Test Rest"
+        cat restSanityTest.log
+
+
+
+        echo "Tightening the Security groups so that the machines can talk to one another."
+        ./tightenSg
+
+        echo "Lastly you must add your infrastructure to the appropriate LOAD BALANCER -- ex. ben-web in aws-west-2"
+
+      else
+       exit
+    fi
+
   else
     printf "\nUpdating your existing infrastructure."
 
