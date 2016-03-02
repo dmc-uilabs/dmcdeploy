50ce28c2f14c41dff4085e5907e085e4607b1248
alpha <alex.maties@uilabs.org>
2016-03-02 16:12:38 -0600

    added start here script

--- createDevStack.sh
+++ createDevStack.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-echo "Use the following script to create your dev stack."
-
 
 echo -n "AWS_ACCESS_KEY_ID [ENTER][q to quit] "
 read name
@@ -37,12 +35,12 @@ case $sec1 in [qQ]) exit;; esac
 export stackPrefix=$sec1
 
 
-echo -n "serverURL [ENTER][q to quit] "
+echo -n "serverURL { leaving blank will default to -- ben-web.opendmc.org } [ENTER][q to quit] "
 read sec2
 if [ -z "$sec2" ]
   then
-    echo "No arguments supplied quitting"
-    exit
+    echo "Setting to default [ ben-web.opendmc.org ]"
+    sec2='ben-web.opendmc.org'
 fi
 case $sec2 in [qQ]) exit;; esac
 export serverURL=$sec2
@@ -81,15 +79,16 @@ export commit_rest=$sec4
  #### will then use the variables above to create the appropriate terraform.tfvars file
 
 
- echo "Will now create a new key pair an place in the ~/keys folder and upload it to aws"
- # create a new key pair or use DMCDriver
- cd /home/ec2-user/dmcdeploy/devtools
- mkdir ~/keys/$stackPrefix
- ./keymaker.sh 1 devStack us-west-2 ~/keys/$stackPrefix $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+ # echo "Will now create a new key pair an place in the ~/keys folder and upload it to aws"
+ # # create a new key pair or use DMCDriver
+ # cd /home/ec2-user/dmcdeploy/devtools
+ # mkdir ~/keys/$stackPrefix
+ # ./keymaker.sh 1 devStack us-west-2 ~/keys/$stackPrefix $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+
+ echo "Will be using the default key pair for this machine"
 
 
-
- kname=$(cat keyNameZ)
+ kname="DMCDriver"
  echo "Your key name is $kname"
 
  echo "Edit terraform.tfvars as appropriate."
@@ -105,28 +104,28 @@ export commit_rest=$sec4
  sed -i.bak "s|stackPrefix = \"\"|stackPrefix = \"$stackPrefix\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_front = \"\"|key_name_front = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_front = \"\"|key_full_path_front = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_front = \"\"|key_full_path_front = \"~/keys/$kname\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_rest = \"\"|key_name_rest = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_rest = \"\"|key_full_path_rest = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_rest = \"\"|key_full_path_rest = \"~/keys/$kname\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_db = \"\"|key_name_db = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_db = \"\"|key_full_path_db = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_db = \"\"|key_full_path_db = \"~/keys/$kname\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_solr = \"\"|key_name_solr = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_solr = \"\"|key_full_path_solr = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_solr = \"\"|key_full_path_solr = \"~/keys/$kname\"|" terraform.tfvars
 
 
  sed -i.bak "s|key_name_activeMq = \"\"|key_name_activeMq = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_activeMq = \"\"|key_full_path_activeMq = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_activeMq = \"\"|key_full_path_activeMq = \"~/keys/$kname\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_stackMon = \"\"|key_name_stackMon = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_stackMon = \"\"|key_full_path_stackMon = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_stackMon = \"\"|key_full_path_stackMon = \"~/keys/$kname\"|" terraform.tfvars
 
  sed -i.bak "s|key_name_dome = \"\"|key_name_dome = \"$kname\"|" terraform.tfvars
- sed -i.bak "s|key_full_path_dome = \"\"|key_full_path_dome = \"~/keys/$stackPrefix/$kname\"|" terraform.tfvars
+ sed -i.bak "s|key_full_path_dome = \"\"|key_full_path_dome = \"~/keys/$kname\"|" terraform.tfvars
 
 
 
@@ -138,13 +137,13 @@ export commit_rest=$sec4
  sed -i.bak "s|commit_front = \"\"|commit_front = \"$commit_front\"|" terraform.tfvars
 
 
- sed -i.bak "s|AWS_ACCESS_KEY_ID = \"\"|AWS_ACCESS_KEY_ID = \"$AWS_ACCESS_KEY_ID\"|" tightenSg.sh
- sed -i.bak "s|AWS_SECRET_ACCESS_KEY = \"\"|AWS_SECRET_ACCESS_KEY = \"$AWS_SECRET_ACCESS_KEY\"|" tightenSg.sh
+ sed -i.bak "s|export AWS_ACCESS_KEY_ID = \"\"|export AWS_ACCESS_KEY_ID = \"$AWS_ACCESS_KEY_ID\"|" tightenSg.sh
+ sed -i.bak "s|export AWS_SECRET_ACCESS_KEY = \"\"|export AWS_SECRET_ACCESS_KEY = \"$AWS_SECRET_ACCESS_KEY\"|" tightenSg.sh
 
 
  myip=$(curl http://ident.me/)
  echo "To download the private key you have just created run the following command to copy it to the local machine of your wish."
- echo " scp  -i \"DMCDriver.pem\" ec2-user@$myip:/home/ec2-user/keys/$stackPrefix/$kname ~/Desktop/"
+ echo " scp  -i \"DMCDriver.pem\" ec2-user@$myip:/home/ec2-user/keys/$kname ~/Desktop/"
 
 
-echo " The next step is to verify your terraform.tfvars file and execute terraform apply.  "
+echo " The next step is to verify your terraform.tfvars file and execute terraform apply."
