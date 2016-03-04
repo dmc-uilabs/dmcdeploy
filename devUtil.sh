0a3f55c31cd8bbd6a028c73e6b876ce8fbea67ac
alpha <alex.maties@uilabs.org>
2016-03-04 10:05:32 -0600

    Removing PII

--- devUtil.sh
+++ devUtil.sh
@@ -23,3 +23,18 @@ ifNotHaveInstall (){
 
     fi
 }
+
+removePII () {
+  printf "Cleanup of Personal Identifiable information"
+  sed -i.bak 's/^access_key = ".*/access_key = ""/' terraform.tfvars
+  sed -i.bak 's/^secret_key = ".*/secret_key = ""/' terraform.tfvars
+
+  sed -i.bak 's/^export AWS_ACCESS_KEY_ID=".*/export AWS_ACCESS_KEY_ID=""/' tightenSgDev.sh
+  sed -i.bak 's/^export AWS_SECRET_ACCESS_KEY=".*/export AWS_SECRET_ACCESS_KEY=""/' tightenSgDev.sh
+  rm terraform.tfvars.bak
+  rm tightenSgDev.sh.bak
+
+
+  printf "AWS Keys have been removed from terraform.tfvars file as well as tightenSgDev.sh. \n Have a smooth day."
+
+}
