7b991e1dcdc840fba70dbf17963a7e4509052714
alpha <alex.maties@uilabs.org>
2016-03-14 17:01:23 -0500

    updating the stackprefix setter code

--- tightenSg.sh
+++ tightenSg.sh
@@ -21,7 +21,7 @@ export AWS_DEFAULT_REGION="us-east-1"
 
 # server connection variables
 
-stackprefix=$(cat terraform.tfstate | jq '.modules[0].resources."aws_instance.front".primary.attributes."tags.Prefix"')
+stackprefix=$(getFromTfVars stackprefix)
 
 front_private_ip=$(cat terraform.tfstate | jq '.modules[0].resources."aws_instance.front".primary.attributes.private_ip')
 #removing quotes from variable
