2b6bb5115919fe44199469fb8fdaa82458d9b163
alpha <alex.maties@uilabs.org>
2016-03-14 16:08:55 -0500

    removed strings

--- tightenSgDev.sh
+++ tightenSgDev.sh
@@ -13,8 +13,8 @@ source ./devUtil.sh
 # install it if not available
 ifNotHaveInstall jq
 
-export AWS_ACCESS_KEY_ID="accesskey"
-export AWS_SECRET_ACCESS_KEY="secretkey"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
 export AWS_DEFAULT_REGION="us-west-2"
 
 
