0321c8042920a8506c3f6ad45524e6507fdbe50a
alpha <alex.maties@uilabs.org>
2016-04-06 15:13:08 -0500

    Added dome 8080 to security group

--- main.tf
+++ main.tf
@@ -126,6 +126,14 @@ resource "aws_security_group" "sg_dome" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP access from anywhere
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # outbound internet access
   egress {
     from_port = 0
