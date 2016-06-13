2643df59546f7c24734b5243e1b2a36b8d64b56e
alpha <alex.maties@uilabs.org>
2016-06-13 06:33:15 -0500

    Refactor code for Azure

--- outputs.tf
+++
@@ -1,5 +0,0 @@
-output "front_public_ip" {
-  value = "${aws_instance.front.public_ip}"
-}
-
-
--- s3/main.tf
+++
@@ -1,9 +0,0 @@
-# Specify the provider and access details
-provider "aws" {
-  region = "${var.aws_region}"
-  access_key = "${var.access_key}" 
-  secret_key = "${var.secret_key}"
-}
-
-
-
