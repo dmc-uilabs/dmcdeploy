7344051ad9f1294827e5dc289903c48e8e81fb50
AlexMaties <alex.maties@uilabs.org>
2016-07-28 19:33:15 +0000

    removed scp

--- aws/rest.tf
+++ aws/rest.tf
@@ -78,8 +78,6 @@ resource "aws_instance" "rest" {
     Name = "${var.stackPrefix}DMC-rest"
   }
 
-  provisioner "local-exec" {
-    command = "scp -oStrictHostKeyChecking=no -i ${var.key_full_path_rest} ec2-user@${aws_instance.rest.public_ip}:/home/ec2-user/restSanityTest.log ."
-  }
+
 
 }
