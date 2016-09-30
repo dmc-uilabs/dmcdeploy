35ac640278c493277c48a7d3b7c59c2832069971
alex maties <alex.maties@uilabs.org>
2016-09-30 09:42:27 -0500

    more missed qoutes

--- aws/dome.tf
+++ aws/dome.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "dome" {
  key_name = "${var.key_name_dome}"
 
   # Our Security group to allow HTTP and SSH access
-  security_groups = [${aws_security_group.sg_wide.name}"]
+  security_groups = ["${aws_security_group.sg_wide.name}"]
 
   # We run a remote provisioner on the instance after creating it.
   #this is where we set the env variables
