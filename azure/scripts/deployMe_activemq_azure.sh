a0f96ab96587968f892be1a4696d074137e3e575
alex maties <alex.maties@uilabs.org>
2017-01-14 15:37:05 -0600

    configured activeMq

---
+++ azure/scripts/deployMe_activemq_azure.sh
@@ -0,0 +1,32 @@
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+sudo yum install -y git java-1.8.0-openjdk
+
+source /etc/profile.d/dmc.sh
+
+cd /tmp
+
+git clone https://github.com/dmc-uilabs/dmcactive.git
+
+cd /tmp/dmcactive
+mv * ..
+
+sudo useradd -m activemq -d /opt/activemq
+sudo chown -R activemq:dmcAdmin /opt/apache-activemq-5.13.4/
+
+sudo cp /opt/activemq/bin/env /etc/default/activemq
+sudo sed -i '~s/^ACTIVEMQ_USER=""/ACTIVEMQ_USER="activemq"/' /etc/default/activemq
+sudo chmod 644 /etc/default/activemq
+
+sudo ln -snf  /opt/activemq/bin/activemq /etc/init.d/activemq
+sudo chkconfig --add activemq
+
+sudo chkconfig activemq on
+
+sudo cp -v  /tmp/jetty-realm.properties /opt/activemq/conf/jetty-realm.properties
+echo "admin: $activeMqRootPass, admin" | sudo tee -a /opt/activemq/conf/jetty-realm.properties
+echo "user: $activeMqUserPass, user" | sudo tee -a /opt/activemq/conf/jetty-realm.properties
+#start ActiveMQ
+sudo service activemq start
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
