b5fe88ff7164e0fc98d5f86a66d64f8829121643
alex maties <alex.maties@uilabs.org>
2017-01-14 13:53:09 -0600

    created the rest machine and the active machine

--- azure/rest.tf
+++ azure/rest.tf
@@ -1,28 +1,28 @@
-resource "azurerm_public_ip" "dbPubIp" {
-    name = "${var.stackPrefix}dbpubip"
+resource "azurerm_public_ip" "restPubIp" {
+    name = "${var.stackPrefix}restpubip"
     location = "${var.azureRegion}"
     resource_group_name = "${azurerm_resource_group.resource.name}"
     public_ip_address_allocation = "static"
 }
 
-resource "azurerm_network_interface" "dbInt" {
-    name = "${var.stackPrefix}dbni"
+resource "azurerm_network_interface" "restInt" {
+    name = "${var.stackPrefix}restni"
     location = "${var.azureRegion}"
     resource_group_name = "${azurerm_resource_group.resource.name}"
 
     ip_configuration {
-        name = "${var.stackPrefix}DbIntNetIntIp"
+        name = "${var.stackPrefix}RestIntNetIntIp"
         subnet_id = "${azurerm_subnet.resource.id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id = "${azurerm_public_ip.dbPubIp.id}"
+        public_ip_address_id = "${azurerm_public_ip.restPubIp.id}"
     }
 }
 
-resource "azurerm_virtual_machine" "db" {
-    name = "dbVm"
+resource "azurerm_virtual_machine" "rest" {
+    name = "restVm"
     location = "${var.azureRegion}"
     resource_group_name = "${azurerm_resource_group.resource.name}"
-    network_interface_ids = ["${azurerm_network_interface.dbInt.id}"]
+    network_interface_ids = ["${azurerm_network_interface.restInt.id}"]
     vm_size = "${var.vmSize}"
     delete_data_disks_on_termination = "true"
     delete_os_disk_on_termination = "true"
@@ -36,8 +36,8 @@ resource "azurerm_virtual_machine" "db" {
 
 
     storage_os_disk {
-        name = "dbOsDisk"
-        vhd_uri = "${azurerm_storage_account.resource.primary_blob_endpoint}${azurerm_storage_container.resource.name}/dbOsDisk.vhd"
+        name = "restOsDisk"
+        vhd_uri = "${azurerm_storage_account.resource.primary_blob_endpoint}${azurerm_storage_container.resource.name}/restOsDisk.vhd"
         caching = "ReadWrite"
         create_option = "FromImage"
     }
