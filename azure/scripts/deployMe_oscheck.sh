1bd7bb0b6e5f9a6b5064205c16a3d53aa51dd76e
Craig Hancock <craig.hancock@uilabs.org>
2017-03-03 12:29:56 -0600

    Adding possibly final suricata config

--- azure/scripts/deployMe_oscheck.sh
+++ azure/scripts/deployMe_oscheck.sh
@@ -8,4 +8,6 @@ fi
 if grep --quiet "^CentOS" /etc/redhat-release; then
   sudo yum -y install git-all
   sudo sed -i 's|#mirror|mirror|' /etc/yum.repos.d/CentOS-Base.repo
+  sudo yum -y install epel-release
+  sudo yum -y install java-1.8.0-openjdk
 fi
