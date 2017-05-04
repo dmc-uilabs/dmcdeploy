ed3f4369555296c6cad55f94ea46e8495a4d7c4e
Jonathan Brand <jhbrand@umich.edu>
2017-05-04 09:54:33 -0500

    added suricata piece

--- azure/scripts/deployMe_secmon_azure.sh
+++ azure/scripts/deployMe_secmon_azure.sh
@@ -14,16 +14,21 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 #     - logstash-forwarder.key
 # NGINX CONFIG FILE FOR KIBANA:
 #     - kibana.conf
-# FILEBEAT CONFIG FILE (specific for stackmon):
-#     - filebeat.yml
-# METRICBEAT CONFIG FILE:
-#     - metricbeat.yml
+# FILEBEAT CONFIG FILE (specific for each server):
+#     - filebeat.yml +
+# METRICBEAT CONFIG FILE (specific for each server):
+#     - metricbeat.yml +
 # ELASTALERT CONFIG FILES:
 #     - config.yaml
 #     - smtp_auth_file.yaml
 #     - supervisord.conf
 # SURICATA CONFIG FILES:
 #     - suricata.yaml
+# + naming convention for metricbeat and filebeat: (metric|file)beat.servername.yml
+
+
+######## GRAB CONFIG FILES ###############
+##########################################
 
 
 ####### INSTALL ELASTICSEARCH ############
@@ -32,6 +37,34 @@ sudo yum install -y java-1.8.0-openjdk
 curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.2.rpm
 sudo rpm -i elasticsearch-5.3.2.rpm
 sudo service elasticsearch start
+##########################################
+
+
+######### CREATE TLS CERTS ###############
+mkdir /tmp/logstash
+cp logstash-ca.conf /tmp/logstash
+cd /tmp/logstash
+openssl req -newkey rsa:2048 -days 3650 -x509 -nodes -out root.crt
+openssl req -newkey rsa:2048 -nodes -out logstash-forwarder.csr -keyout logstash-forwarder.key
+echo 000a > serialfile
+touch certindex
+
+STACKMON_IP=$(ifconfig eth0 | grep "inet " | cut -d ' ' -f 10)
+
+sed -i 's/subjectAltName.*/subjectAltName = IP:'$STACKMON_IP'/' logstash-ca.conf
+openssl ca -batch -config logstash-ca.conf -notext -in logstash-forwarder.csr -out logstash-forwarder.crt
+sudo mv logstash-forwarder.crt /etc/pki/tls/certs
+sudo mv logstash-forwarder.key /etc/pki/tls/private
+
+sudo yum install -y ca-certificates
+sudo update-ca-trust force-enable
+sudo mv root.crt /etc/pki/ca-trust/source/anchors
+sudo update-ca-trust extract
+
+cd ..
+rm -r logstash
+
+# Permissions?
 ##########################################
 
 
@@ -45,12 +78,13 @@ sudo sed -i 's/LS_OPTS=.*/LS_OPTS=\"--path.settings ${LS_SETTINGS_DIR} --log.lev
 sudo /usr/share/logstash/bin/system-install
 
 # Copy configuration to logstash config folder
-sudo mv 02-filebeat-input.conf /etc/logstash/conf.d # ALEX: mv or cp?
+sudo mv 02-filebeat-input.conf /etc/logstash/conf.d # ALEX: mv or scp from s3 bucket (do that in terra or here)?
 sudo mv 10-syslog-filter.conf /etc/logstash/conf.d
 sudo mv 10-suricata-filter.conf /etc/logstash/conf.d
 sudo mv 30-elasticsearch-output.conf /etc/logstash/conf.d
 
 # Copy ssl certs to system certs directory
+# ALEX: responsibility of this machine to create certs and put in bucket for other machines
 sudo mv logstash-forwarder.crt /etc/pki/tls/certs
 sudo mv logstash-forwarder.key /etc/pki/tls/private
 
@@ -87,13 +121,15 @@ sudo setenforce 0 # ALEX: is this okay?
 sudo service nginx start
 ##########################################
 
+# ALEX: (stackmon.opendmc.org - update cloudflare dns entry with public ip)
+
 
 ######## INSTALL FILEBEAT ################
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.3.2-x86_64.rpm
 sudo rpm -vi filebeat-5.3.2-x86_64.rpm
 
 # Copy config for filebeat and change permissions for filebeat constraints
-sudo mv filebeat.yml /etc/filebeat/ # ALEX: local address needs to be consistent (currently 10.0.14.6); needed for logstash-forwarded cert as well
+sudo mv filebeat.yml /etc/filebeat/ # ALEX: local address needs to be consistent (currently 10.0.14.6); needed for logstash-forwarder cert as well
 sudo chmod 644 /etc/filebeat/filebeat.yml
 sudo chown root /etc/filebeat/filebeat.yml
 sudo chgrp root /etc/filebeat/filebeat.yml
@@ -166,9 +202,8 @@ sudo pip install twilio==6.0.0
 # Create elasalert index in elasticsearch
 echo -e "localhost\n9200\nf\n\n\n\n\n\n" | elastalert-create-index
 
-cd ~
-
 # Copy config files to elastalert directory
+cd ~
 mv config.yaml /etc/elastalert
 mv smtp_auth_file.yaml /etc/elastalert
 
@@ -183,4 +218,6 @@ supervisord
 
 ######### INSTALL NESSUS #################
 ##########################################
+
+# Cert creation, packetbeat
 
