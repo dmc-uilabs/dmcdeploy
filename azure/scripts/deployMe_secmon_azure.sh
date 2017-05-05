66cb5a33d731c26f713cc4feb0cfd6fc039cfab9
Jonathan Brand <jhbrand@umich.edu>
2017-05-05 11:45:00 -0500

    fixed nessus

--- azure/scripts/deployMe_secmon_azure.sh
+++ azure/scripts/deployMe_secmon_azure.sh
@@ -245,14 +245,19 @@ supervisord
 ######### INSTALL NESSUS #################
 curl -s -L https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip | funzip | sudo tee /usr/local/bin/pup >/dev/null; sudo chmod 755 /usr/local/bin/pup
 TOKEN=`curl -s https://www.tenable.com/products/nessus/agent-download | pup 'div#timecheck text{}'`
-curl -L -s "http://downloads.nessus.org/nessus3dl.php?file=Nessus-6.10.5-es7.x86_64.rpm&licence_accept=yes&t=$TOKEN" -o /tmp/nessus.rpm
-sudo rpm -i /tmp/nessus.rpm
-#sudo /opt/nessus/sbin/nessus-service nessusagent start
-sudo /opt/nessus/sbin/nessuscli managed link --key=$NESSUS_KEY --group="group1" --port=443 --host=cloud.tenable.com --port=443
-#sudo systemctl enable nessusd
-#sudo systemctl start nessusd
+# curl -L -s "http://downloads.nessus.org/nessus3dl.php?file=NessusAgent-6.10.5-es7.x86_64.rpm&licence_accept=yes&t=$TOKEN" -o /tmp/nessus.rpm
+# sudo rpm -i /tmp/nessus.rpm
+# #sudo /opt/nessus/sbin/nessus-service nessusagent start
+# sudo /opt/nessus/sbin/nessuscli managed link --key=$NESSUS_KEY --group="group1" --port=443 --host=cloud.tenable.com --port=443
+# #sudo systemctl enable nessusd
+# #sudo systemctl start nessusd
+# 
+# #sudo /sbin/service nessusagent start
+# sudo service nessusd start
 
-#sudo /sbin/service nessusagent start
-sudo service nessusd start
+curl -L -s "http://downloads.nessus.org/nessus3dl.php?file=NessusAgent-6.10.5-es7.x86_64.rpm&licence_accept=yes&t=$TOKEN" -o /tmp/nessus.rpm
+sudo rpm -i /tmp/nessus.rpm
+sudo /opt/nessus_agent/sbin/nessuscli agent link --key=$NESSUS_KEY --group="group1" --port=443 --host=cloud.tenable.com
+sudo /bin/systemctl start nessusagent.service
 ##########################################
 
