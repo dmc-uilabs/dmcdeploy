4098f8cb3ecd6aa10b9941403db0e74b009d5964
Jonathan Brand <jhbrand@umich.edu>
2017-05-05 15:46:49 -0500

    Updated connector script

--- azure/scripts/connectTo_secmon_azure.sh
+++ azure/scripts/connectTo_secmon_azure.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -v
 #anything printed on stdout and stderr to be sent to the syslog1, as well as being echoed back to the original shell’s stderr.
 exec 1> >(logger -s -t $(basename $0)) 2>&1
-# Need: filebeat.yml, metricbeat.yml, root.crt
+
+# THIS SCRIPT REQUIRES THE FOLLOWING:
+# FILES:
+#    - filebeat.<hostname>.yml
+#    - metricbeat.<hostname>.yml
+#    - root.crt
+# ENV VARS:
+#    - NESSUS_KEY
 
 ######## GRAB CONFIG FILES ###############
 # Grab files from S3 bucket
 # Change (metric|file)beat.servername.yml to (metric|file)beat.yml
-mv metricbeat.secmon.yml metricbeat.yml
-mv filebeat.secmon.yml filebeat.yml
+HOSTNAME=$(hostname | cut -d. -f1)
+mv metricbeat.$HOSTNAME.yml metricbeat.yml
+mv filebeat.$HOSTNAME.yml filebeat.yml
 #mv packetbeat.secmon.yml packetbeat.yml
 
 # Install CA cert for communication with logstash
@@ -43,4 +51,14 @@ sudo chown root /etc/metricbeat/metricbeat.yml
 sudo chgrp root /etc/metricbeat/metricbeat.yml
 
 sudo service metricbeat start
+##########################################
+
+
+######### INSTALL NESSUS #################
+curl -s -L https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip | funzip | sudo tee /usr/local/bin/pup >/dev/null; sudo chmod 755 /usr/local/bin/pup
+TOKEN=`curl -s https://www.tenable.com/products/nessus/agent-download | pup 'div#timecheck text{}'`
+curl -L -s "http://downloads.nessus.org/nessus3dl.php?file=NessusAgent-6.10.5-es7.x86_64.rpm&licence_accept=yes&t=$TOKEN" -o /tmp/nessus.rpm
+sudo rpm -i /tmp/nessus.rpm
+sudo /opt/nessus_agent/sbin/nessuscli agent link --key=$NESSUS_KEY --group="group1" --port=443 --host=cloud.tenable.com
+sudo /bin/systemctl start nessusagent.service
 ##########################################
