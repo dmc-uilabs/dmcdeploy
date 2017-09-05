edb9ff55e1f1e4901cd6dd5a17415eb3d498490a
Jonathan Brand <jonathan.brand@uilabs.org>
2017-09-05 16:00:55 -0500

    add sendGrid config to validate deploy script

--- azure/fullenv/scripts/deployMe_validate_azure.sh
+++ azure/fullenv/scripts/deployMe_validate_azure.sh
@@ -10,3 +10,10 @@ source /etc/profile.d/dmc.sh
 npm install
 
 pm2 start validate.js -i max
+
+
+### Sendgrid for user validation emails
+cd /opt
+git clone https://github.com/dmc-uilabs/dmcEmail.git
+npm install
+pm2 start sendGrid.js -i max
